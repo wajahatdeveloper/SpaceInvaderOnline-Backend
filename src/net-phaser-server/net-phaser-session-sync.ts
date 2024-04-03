7a41de432bc7c6eb7b7e31d8dbbe4fd30c4514c3
wajahatdeveloper <58264405+wajahatdeveloper@users.noreply.github.com>
2024-04-03 12:29:41 +0500

    send receive data

--- src/net-phaser-server/net-phaser-session-sync.ts
+++ src/net-phaser-server/net-phaser-session-sync.ts
@@ -2,12 +2,17 @@ import { Server, Socket } from "socket.io";
 import { NetEvent, eventManager } from ".";
 
 function sendDataToAll(playerSocket: Socket, socketServer: Server, clientId: string, sessionId: string, data: any) {
-    socketServer.to(sessionId).emit("receiveData", {clientId, data});
+    console.log(`dataSentToAll from ${clientId}, data: ${JSON.stringify(data)}`);
+    
+    const fromClientId = clientId;
+    socketServer.to(sessionId).emit("receiveData", fromClientId, data);
     eventManager.triggerCallback(NetEvent.OnDataSent, {playerSocket, data})
 }
 
 function receiveData(fromClientId: string, data: any) {
     eventManager.triggerCallback(NetEvent.OnDataReceived, {fromClientId, data});
+
+    console.log(`receivedData from ${fromClientId}, data: ${JSON.stringify(data)}`);
 }
 
 export {
